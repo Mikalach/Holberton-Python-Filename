0d45dbdd466b2eef77bf572be118ae3f4cbf695d
AlexandreGRN <tulkiidra@gmail.com>
2023-01-24 08:40:34 -0800

    Stable 1.0

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-fileIn = open("C", "r")
+fileIn = open("SourceCod", "r")
+Lines = fileIn.readlines()
+Suppr = ["<li>","\n","<code>", "</li>"," ","</code>"]
+Search = "File:"
 
-print(fileIn.read(5))
+def createandwrite(name):
+    f = open(name, "x")
+    f.write("#!/usr/bin/python3")
+    f.close()
+
+for line in Lines:
+    for char in Suppr:
+        line = line.replace(char, "")
+    if Search in line:
+        line = line.replace(Search, "")
+        createandwrite(line)
